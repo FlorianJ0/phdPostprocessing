439c46e985591b874f63e70c7b13116591124dbc
pueponey@gmail.com <so7Rooca>
2018-02-06 15:05:31 +0100

    added nn plots ok

--- stat_phd.py
+++ stat_phd.py
@@ -6,31 +6,20 @@ Created on Sat Nov 25 18:59:47 2017
 @author: florian
 """
 
-import os
-from scipy import stats
-import numpy as np
-import matplotlib.pyplot as plt
-import collections
-import matplotlib.cm as cm
-import pandas as pd
-from bokeh.layouts import gridplot
-from bokeh.plotting import figure, show, output_file
-import traces
-import seaborn as sns
-from bokeh.models import LinearAxis, Range1d, ContinuousAxis
-from bokeh.models import ColumnDataSource, Whisker
-from bokeh.layouts import gridplot
-from bokeh.io import output_file, show, export_png, export_png
-from bokeh.layouts import gridplot, layout
-from bokeh.palettes import Viridis3
-from bokeh.plotting import figure
-import numpy as np
 # import holoviews as hv
 import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import traces
 import vapeplot
+from bokeh.models import LinearAxis, Range1d
+from bokeh.plotting import figure
+from scipy import stats
 
 # vapeplot.set_palette('sunset')
-pal = sns.blend_palette(vapeplot.palette('vaporwave'))
+pal = sns.blend_palette(vapeplot.palette('sunset'))
 # hv.extension('bokeh')
 
 localData = np.load("localdata.npy")
@@ -94,15 +83,19 @@ globalVarNames = ['lastScan', 'patientId', 'scanId', 'ptName', 'AAA', 'Age (year
 
 # print globalVarNames
 
-sortList = [0,1,2,3,4,6,5,7,8,9,10,11,12,13,19,29,14,28,15,34,16,17,21,31,24,32,20,30,35,36,37,38,95,96,45,53,61,69,41,49,57,65,44,52,60,68,40,48,56,64,43,51,59,67,46,54,62,70,72,73,71,75,76,74,84,85,83,90,91,89,81,82,80,87,88,86,93,94,92,77,78,79,66,63,58,55,50,47,42,18,22,23,25,26,27,33,39,97,98,99,100]
+sortList = [0, 1, 2, 3, 4, 6, 5, 7, 8, 9, 10, 11, 12, 13, 19, 29, 14, 28, 15, 34, 16, 17, 21, 31, 24, 32, 20, 30, 35,
+            36, 37, 38, 95, 96, 45, 53, 61, 69, 41, 49, 57, 65, 44, 52, 60, 68, 40, 48, 56, 64, 43, 51, 59, 67, 46, 54,
+            62, 70, 72, 73, 71, 75, 76, 74, 84, 85, 83, 90, 91, 89, 81, 82, 80, 87, 88, 86, 93, 94, 92, 77, 78, 79, 66,
+            63, 58, 55, 50, 47, 42, 18, 22, 23, 25, 26, 27, 33, 39, 97, 98, 99, 100]
 
 globalVarNamesSorted = []
 globalDataSorted = np.empty_like(globalData)
+
 for i in xrange(len(globalVarNames)):
     # i += 1
     # print i, globalVarNames[i], sortList[i], globalVarNames[sortList[i]],'\n'
     globalVarNamesSorted.append(globalVarNames[sortList[i]])
-    if i<100:
+    if i < 100:
         globalDataSorted[:, i] = globalData[:, sortList[i]]
 globalVarNames = globalVarNamesSorted
 
@@ -110,7 +103,6 @@ globalData = globalDataSorted
 # print globalVarNames
 # print globalDataSorted[:,5]
 # print globalData[:,25]
-#
 
 localVarNames = ['AbscissaMetric', 'AngularMetric', 'BoundaryMetric', 'ClippingArray', \
                  'DistanceToCenterlines', 'Divergence_average', 'Gradients_average', \
@@ -120,7 +112,7 @@ localVarNames = ['AbscissaMetric', 'AngularMetric', 'BoundaryMetric', 'ClippingA
                  'localRRTVar', 'localTAWSSVar', 'localECAPVar', 'ECAP', 'localDistToCtrl', \
                  'localDistToCtrlVar', 'thrombusGrowthThresh', 'stretchGrowthThresh', 'disToCtrlGrowthThresh']
 
-AAAvsnoAAA = 1
+AAAvsnoAAA = 0
 riskvsnorisk = 0
 # print globalData[:,4], globalVarNames[4]
 
@@ -145,9 +137,17 @@ for i in xrange(globalDataAAA.shape[0]):
         globalDataAAAnotRisky = np.delete(globalDataAAAnotRisky, i - nrowremoved, 0)
         nrowremoved += 1
 
+plot = globalDataAAAnotRisky[:, 25].astype(float)[~np.isnan(globalDataAAAnotRisky[:, 25].astype(float))]
+plot3 = globalDataAAARisky[:, 25].astype(float)[~np.isnan(globalDataAAARisky[:, 25].astype(float))]
+
+sns.distplot(plot.astype(float))
+sns.distplot(plot3.astype(float))
+
+plt.show()
+
 if AAAvsnoAAA:
     for i in xrange(globalData.shape[1]):
-        if i not in [0, 1, 3,4, 5,7, 89, 92, 95, 98, 99, 100]:
+        if i not in [0, 1, 3, 4, 5, 7, 89, 92, 95, 98, 99, 100]:
             aaa = globalDataAAA[:, i].astype(float)
             noaaa = globalDatanoAAA[:, i].astype(float)
             #        print pdcolumnlist[i]
@@ -157,7 +157,8 @@ if AAAvsnoAAA:
             s, p = stats.ttest_ind(aaa, noaaa, equal_var=False, nan_policy='omit')
             if p < 0.001:
                 p = '<0.001'
-            print globalVarNames[i],',',np.nanmean(aaa),',',np.nanstd(aaa), ',',np.nanmean(noaaa),',',np.nanstd(noaaa),',',p
+            print globalVarNames[i], '\t', np.nanmean(aaa), '\t', np.nanstd(aaa), '\t', np.nanmean(
+                noaaa), '\t', np.nanstd(noaaa), '\t', p
     print '\n\n'
 
     for i in xrange(localData.shape[1]):
@@ -175,7 +176,7 @@ if AAAvsnoAAA:
 
 if riskvsnorisk:
     for i in xrange(globalData.shape[1]):
-        if i not in [0, 1, 3,4, 5,7, 89, 92, 95, 98, 99, 100]:
+        if i not in [0, 1, 3, 4, 5, 7, 89, 92, 95, 98, 99, 100]:
             # print i, globalVarNames[i]
             risk = globalDataAAARisky[:, i].astype(float)
             norisk = globalDataAAAnotRisky[:, i].astype(float)
@@ -186,7 +187,8 @@ if riskvsnorisk:
             s, p = stats.ttest_ind(risk, norisk, equal_var=False, nan_policy='omit')
             # if p < 0.001:
             #     p = '<0.001'
-            # print globalVarNames[i],',',np.nanmean(risk),',',np.nanstd(risk), ',',np.nanmean(norisk),',',np.nanstd(norisk),',',p
+            print globalVarNames[i], '\t', np.nanmean(risk), '\t', np.nanstd(risk), '\t', np.nanmean(
+                norisk), '\t', np.nanstd(norisk), '\t', p
     print '\n\n'
 
 nscans = []
@@ -217,60 +219,70 @@ a = np.split(globalData, nscans, axis=0)
 
 time_seriesSlow = traces.TimeSeries()
 time_seriesFast = traces.TimeSeries()
+plotGlobal = 0
+if plotGlobal:
+    for varID in xrange(98):
+        # varID = 28
+        if varID not in [0, 1, 3, 4, 5, 7, 15, 89, 92, 95, 98, 99, 100]:
+            varname = globalVarNames[varID]
+            outname = varname + '.png'
+            print 'varname', varname, varID
 
-varID = 39
-varname = globalVarNames[varID]
-print 'varname', varname
+            minVar = np.nanmin(globalData[:, varID].astype(float)) - 0.1 * np.nanmin(globalData[:, varID].astype(float))
+            maxVar = np.nanmax(globalData[:, varID].astype(float)) + 0.1 * np.nanmax(globalData[:, varID].astype(float))
+            print minVar, maxVar
 
-minVar = np.nanmin(globalData[:, varID].astype(float)) - 0.1 * np.nanmin(globalData[:, varID].astype(float))
-maxVar = np.nanmax(globalData[:, varID].astype(float)) + 0.1 * np.nanmax(globalData[:, varID].astype(float))
-print minVar, maxVar
+            p1 = figure(x_range=(-1, np.max(globalData[:, -1].astype(float) + 1)), y_range=(minVar, maxVar))
+            p1.grid.grid_line_alpha = 0.3
+            p1.xaxis.axis_label = 'Follow-up time (month)'
+            p1.yaxis.axis_label = varname
+            p1.extra_y_ranges = {"foo": Range1d(start=0, end=1)}
+            p1.add_layout(LinearAxis(y_range_name="foo", axis_label='Normalized averaged data over all patients'),
+                          'right')
+            # p1.add_layout(LinearAxis(y_range_name="toto"), 'above')
 
-p1 = figure(x_range=(-1, np.max(globalData[:, -1].astype(float) + 1)), y_range=(minVar, maxVar))
-p1.grid.grid_line_alpha = 0.3
-p1.xaxis.axis_label = 'Follow-up time (month)'
-p1.yaxis.axis_label = varname
-p1.extra_y_ranges = {"foo": Range1d(start=0, end=1)}
-p1.add_layout(LinearAxis(y_range_name="foo", axis_label='Normalized averaged data over all patients'), 'right')
-# p1.add_layout(LinearAxis(y_range_name="toto"), 'above')
+            for i in xrange(np.max(globalData[:, 1].astype(int))):
+                # b= np.append(a[i][:,0], np.indices([a[i][:,0].shape[0]]).T.reshape(a[i][:,0].shape[0]), axis=1)
+                index = list(np.indices([a[i][:, 0].shape[0]])[0])
+                df2 = pd.DataFrame(a[i][:, [varID, -3, -1]], index=index)
+                if df2[1][0] == 'True':
+                    for k in xrange(a[i][:, -1].shape[0]):
+                        time_seriesFast[a[i][k, -1].astype(float)] = (a[i][k, varID].astype(float) - minVar) / (
+                                maxVar - minVar)
+                    color = '#ffb3b3'
+                    # print 'Grand'
+                else:
+                    for k in xrange(a[i][:, -1].shape[0]):
+                        time_seriesSlow[a[i][k, -1].astype(float)] = (a[i][k, varID].astype(float) - minVar) / (
+                                maxVar - minVar)
 
-for i in xrange(np.max(globalData[:, 1].astype(int))):
-    # b= np.append(a[i][:,0], np.indices([a[i][:,0].shape[0]]).T.reshape(a[i][:,0].shape[0]), axis=1)
-    index = list(np.indices([a[i][:, 0].shape[0]])[0])
-    df2 = pd.DataFrame(a[i][:, [varID, -3, -1]], index=index)
-    if df2[1][0] == 'True':
-        for k in xrange(a[i][:, -1].shape[0]):
-            time_seriesFast[a[i][k, -1].astype(float)] = (a[i][k, varID].astype(float) - minVar) / (maxVar - minVar)
-        color = '#ffb3b3'
-        # print 'Grand'
-    else:
-        for k in xrange(a[i][:, -1].shape[0]):
-            time_seriesSlow[a[i][k, -1].astype(float)] = (a[i][k, varID].astype(float) - minVar) / (maxVar - minVar)
+                    color = '#b3b3ff'
+                if i > 0:
+                    p1.line(df2[2], df2[0], color=color, line_width=4)
+                    # print df2[0]
 
-        color = '#b3b3ff'
-    if i > 0:
-        p1.line(df2[2], df2[0], color=color, line_width=4)
-        # print df2[0]
+            quickAvg = time_seriesFast.moving_average(1, window_size=30, pandas=True)
+            quickAvg = pd.DataFrame(quickAvg, columns=[varname])
 
-quickAvg = time_seriesFast.moving_average(1, window_size=30, pandas=True)
-quickAvg = pd.DataFrame(quickAvg, columns=[varname])
+            slowAvg = time_seriesSlow.moving_average(1, window_size=30, pandas=True)
+            slowAvg = pd.DataFrame(slowAvg, columns=[varname])
 
-slowAvg = time_seriesSlow.moving_average(1, window_size=30, pandas=True)
-slowAvg = pd.DataFrame(slowAvg, columns=[varname])
+            p1.line(quickAvg.index, quickAvg[varname], color='#ff0000', line_width=4, y_range_name="foo")
+            p1.line(slowAvg.index, slowAvg[varname], color='#0000ff', line_width=4, y_range_name="foo")
 
-p1.line(quickAvg.index, quickAvg[varname], color='#ff0000', line_width=4, y_range_name="foo")
-p1.line(slowAvg.index, slowAvg[varname], color='#0000ff', line_width=4, y_range_name="foo")
+            p2 = figure()
+            p2.line(slowAvg.index, slowAvg[varname], color='#f142f4', line_width=4, y_range_name="foo")
 
-p2 = figure()
-p2.line(slowAvg.index, slowAvg[varname], color='#f142f4', line_width=4, y_range_name="foo")
+            #
+            # show(p1)
+            # show(p2)
 
-#
-# show(p1)
-# show(p2)
-
-# grid = gridplot([p1, p2], ncols=2, plot_width=250, plot_height=250)
-# show(grid)
-
+            # grid = gridplot([p1, p2], ncols=2, plot_width=500, plot_height=500)
+            # # show(p1 )
+            # try:
+            #     export_png(p1, filename=outname)
+            # except:
+            #     print 'toto'
 varID = 9
 varname = localVarNames[varID]
 
@@ -298,10 +310,9 @@ indices = list(np.indices([600])[0])
 # for ic in range(2):
 #     for il in range(2):
 #         ax.append(fig.add_subplot(4,ic+1,il+1))
-
-sns.set_style("ticks")
+sns.set_style("white")
 sns.set_context("paper")
-
+sns.despine()
 figsize = (6, 6)
 cols = 6
 cases = [1, 2, 3]
@@ -309,6 +320,7 @@ gs = gridspec.GridSpec((np.max(globalData[:, 1].astype(int)) + 5) // cols + 1, c
 fig1 = plt.figure(num=1, figsize=figsize)
 ax = []
 fig1.subplots_adjust(hspace=.5)
+
 j = 0
 # print globalData[:, -1]
 # print globalData[:, -2]
@@ -323,6 +335,10 @@ rc = [(0, 0),
       (5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5),
       (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5)]
 
+varID = 28
+varname = localVarNames[varID]
+outfile = varname + 'local.png'
+print varname, varID
 for i in xrange(np.max(globalData[:, 1].astype(int))):
     index = list(np.indices([a[i][:, 0].shape[0]])[0])
     ll = [x + l for x in index]
@@ -337,17 +353,31 @@ for i in xrange(np.max(globalData[:, 1].astype(int))):
 
     row = (i // cols)
     col = i % cols
+
     if (row, col) == (0, 0):
         ax.append(fig1.add_subplot(gs[0, :3]))
-        toto = sns.boxplot(data=df, width=0.5, showfliers=False, linewidth=0.2, palette=pal)
+        tata = sns.boxplot(data=df, width=0.5, showfliers=False, linewidth=0.2, palette=pal)
+        tata.set_title('Healthy patients', fontsize='xx-small')
+        tata.tick_params(labelbottom='off')
+
+        # tata.set(ylim=(0, 0.5))
 
     else:
-        ax.append(fig1.add_subplot(gs[rc[i]]))
+        if col == 1:
+            ax.append(fig1.add_subplot(gs[rc[i]], sharey=ax[0]))
+            toto = sns.boxplot(data=df, width=0.5, showfliers=False, linewidth=0.2, palette=pal)
+            toto.set_title(str(i), fontsize='xx-small')
 
-        toto = sns.boxplot(data=df, width=0.5, showfliers=False, linewidth=0.2, palette=pal)
-    toto.tick_params(labelbottom='off')
-    toto.set(ylim=(0, 0.5))
+            toto.tick_params(labelbottom='off')
+            # toto.set(ylim=(0, 0.5))
+        else:
+            ax.append(fig1.add_subplot(gs[rc[i]], sharey=ax[0]))
+            tutu = sns.boxplot(data=df, width=0.5, showfliers=False, linewidth=0.2, palette=pal)
+            tutu.set_title(str(i), fontsize='xx-small')
 
+            tutu.tick_params(labelbottom='off')
+            tutu.tick_params(labelleft='off')
+    #
     '''
     if (row,col) == (0,0):
         #     ax.append( fig1.add_subplot(gs[row, col]))
@@ -372,7 +402,9 @@ for i in xrange(np.max(globalData[:, 1].astype(int))):
 
 # ax.plot(pl[0])
 # plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+
 # plt.show()
+plt.savefig(outfile, dpi=300)
 
 # print pl
 # plot_opts = dict(show_legend=False, width=400)
